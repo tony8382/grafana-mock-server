fccce5895e4f303fec472254a36a0264fcaca059
Li-Yu, Yang <tony830802@gmail.com>
2023-05-01 16:00:50 +0800

    init fastify

--- src/app/api/grafana/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
-
-export async function OPTIONS(request: Request) {
-  return new Response('Hello, Next.js!')
-}
-
--- utils/cors.ts
+++
@@ -1,8 +0,0 @@
-export const getCorsHeader = ():Headers =>{
-    let header = new Headers()
-    header.set("Access-Control-Allow-Headers","accept, content-type")
-    header.set("Access-Control-Allow-Methods","POST, OPTIONS")
-    header.set("Access-Control-Allow-Origin","*")
-    
-    return header
-}
